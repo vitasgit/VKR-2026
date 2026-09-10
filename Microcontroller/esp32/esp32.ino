719ad6da60a92e05d93d2f7799e45d8de36613b9
vitasgit <vibrantfeynman@gmail.com>
2026-09-10 21:17:29 +0500

    esp32 server, прием запросов

--- Microcontroller/esp32/esp32.ino
+++ Microcontroller/esp32/esp32.ino
@@ -1,12 +1,13 @@
 #include "WiFi.h"
 #include "myPasswords.h"
 
-// ------------!!!! пароли ак---------------
+// ------------!!!! пароли ---------------
 const char* ssid = MY_SSID;
 const char* password = MY_PASSWORD;
 
 int ledPin = 2;
 
+WiFiServer EspServer(80);  // HTTP
 void setup() {
   pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
@@ -20,15 +21,32 @@ void setup() {
 
   Serial.println("IP: ");
   Serial.println(WiFi.localIP());
+  EspServer.begin();
 
   digitalWrite(ledPin, HIGH);
+
+  //char[256] data;
 }
 
 void loop() {
-  while (WiFi.status() != WL_CONNECTED) { delay(1000); WiFi.begin(ssid, password);}
+  WiFiClient client = EspServer.available();
 
-  digitalWrite(ledPin, LOW);
-  delay(1000);
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
+  if (client) {
+    String request = client.readString();
+
+    if (request.indexOf("GET /?command=1") != -1) {
+      Serial.write('1');
+      digitalWrite(ledPin, HIGH);
+      client.println("HTTP/1.1 200 OK");
+      client.println();
+    }
+    else if (request.indexOf("GET /?command=0") != -1) {
+      Serial.write('0');
+      digitalWrite(ledPin, LOW);
+      client.println("HTTP/1.1 200 OK");
+      client.println();
+    }
+
+    client.stop();
+  }
 }
